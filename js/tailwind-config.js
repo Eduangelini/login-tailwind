2088ed96f17b39c1801957ed683726e8ba1df7a6
Eduardo <eduangelinicarvalho@gmail.com>
2023-03-29 15:39:12 -0300

    commit(#1)

---
+++ js/tailwind-config.js
@@ -0,0 +1,35 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'first': 'rgb(50, 44, 235)',
+        'second': 'rgb(45, 73, 168)',
+        'details': 'rgb(124, 62, 240)',
+        'fourth': 'rgb(62, 229, 240)',
+        'fifth': 'rgb(43, 160, 168)',
+
+        'primary': '#01013F',
+        'secondary': '#BFBEC6',
+
+        'background-1': '#EEEEFA',
+        'button-background': '#F8F7FD',
+        'button-background-2': '#EEEEFA',
+        'details': 'rgb(124, 62, 240)',
+
+        'border-up': '#FEFEFF',
+        'border-down': '#D8D7E5',
+
+        'black': '#000000',
+        'white': '#ffffff',
+      },
+      fontFamily: {
+        'sans': ['Nunito', 'sans-serif'],
+        'serif': ['Noto', 'serif'],
+      },
+      boxShadow: {
+        'shadow': '-20px -20px 20px #FEFEFF, 20px 20px 20px #D8D7E5',
+        'shadow-2': '-5px -5px 20px #FEFEFF, 5px 5px 20px #D8D7E5',
+      },
+    }
+  }
+}
